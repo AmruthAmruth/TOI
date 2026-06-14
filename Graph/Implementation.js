4f126baff57c8b97db697b27c35c765c062cd2f3
Amruth Shyju <amruthshyju@gmail.com>
2026-06-14 19:03:44 +0530

    graph added new func

--- Graph/Implementation.js
+++ Graph/Implementation.js
@@ -18,8 +18,60 @@ class Graph{
     }
 
 
+    removeEdge(v1,v2){
+        this.list[v1]=this.list[v1].filter(vertex=>vertex !==v2)
+        this.list[v2]=this.list[v2].filter(vertex=>vertex!==v1)
+    }
+
+    removeVertex(vertex){
+        while(this.list[vertex].length){
+            let adjecentVertex=this.list[vertex].pop()
+            this.removeEdge(vertex,adjecentVertex)
+        }
+        delete this.list[vertex]
+    }
 
 
+
+    dfs(start){
+        let result=[];
+        let visited={};
+
+        const traverse=(vertex)=>{
+            if(!vertex) return;
+            visited[vertex]=true;
+            result.push(vertex);
+
+            for(let neighbor of this.list[vertex]){
+                if(!visited[neighbor]){
+                    traverse(neighbor)
+                }
+            }
+        }
+        traverse(start)
+        return result
+    }
+
+
+    bfs(start){
+        let queue=[start];
+        let visited={};
+        let result=[];
+
+        visited[start]=true;
+        while(queue.length){
+            let current = queue.shift();
+            result.push(current);
+
+            for(let neighbor of this.list[current]){
+                if(!visited[neighbor]){
+                    visited[neighbor]=true;
+                    queue.push(neighbor)
+                }
+            }
+        }
+        return result
+    }
 
 
 }
@@ -33,4 +85,5 @@ graph.addVertex("C")
 graph.addEdge("A","B")
 graph.addEdge("A","C")
 
-console.log(graph.list)
+// console.log(graph.list)
+console.log(graph.bfs("A"));
